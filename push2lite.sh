9058f800c6996c6260c01324b48ddd8f5bf52f3e
liao <liao961120@gmail.com>
2019-11-18 20:00:03 +0800

    update

--- push2lite.sh
+++ push2lite.sh
@@ -1,1 +1,1 @@
-git branch -D lite && git checkout -b lite && rm -r data-raw/ inst/ docs/ pkgdown/ && git add . && git commit -m 'push to branch "lite"' && git push -f origin lite && git checkout master
+git branch -D lite && git checkout -b lite && rm -r data-raw/ inst/ docs/ pkgdown/ _pkgdown.yml push2lite.sh && git add . && git commit -m 'push to branch "lite"' && git push -f origin lite && git checkout master
